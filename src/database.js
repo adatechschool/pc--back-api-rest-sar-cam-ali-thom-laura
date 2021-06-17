1ce674f6764e6973c3f636d887551b22102cac67
alinabudukina <alinabudukina@gmail.com>
2021-06-17 16:53:03 +0200

    Add database connection script

--- src/database.js
+++ src/database.js
@@ -1,5 +1,22 @@
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("new.db");
-var md5 = require("md5");
+// const sqlite3 = require("sqlite3").verbose();
+// const db = new sqlite3.Database("new.db");
+// var md5 = require("md5");
 
-module.exports = db;
+const mysql = require("mysql");
+const dbConfig = require("db.config.js");
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB,
+});
+
+// open the MySQL connection
+connection.connect((error) => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
+});
+
+module.exports = connection;
